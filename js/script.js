ff777cd71aab7fa8090dd5f58b39a64e97012699
Malar <malarvizhiravichandran8@gmail.com>
2025-02-16 00:00:24 +0530

    Malar's Portfolio code

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Typing animation
+
+var typed = new Typed(".role",{
+    strings: ["","Web Designer", "Web Developer", "Full Stack Developer", "MERN Stack Developer"],
+    typeSpeed: 120,
+    BackSpeed: 80,
+    loop: true,
+})
